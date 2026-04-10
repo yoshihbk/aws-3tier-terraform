382f236567cc9598e554c0b57d197991040e84e8
yoshihbk <yokoyamayoshihiro1995@outlook.jp>
2026-04-10 18:34:02 +0900

    Add IGW and public route table

---
+++ terraform/route_tables.tf
@@ -0,0 +1,31 @@
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.default_tags,
+    {
+      Name = "igw"
+    }
+  )
+}
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = merge(
+    var.default_tags,
+    {
+      Name = "public-rt"
+    }
+  )
+}
+resource "aws_route_table_association" "public" {
+  for_each = aws_subnet.public
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public.id
+}
